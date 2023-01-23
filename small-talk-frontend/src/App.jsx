f928d944b031fa84414a4cdd2ea934b3eb0440a5
unknown <staracenapuga@gmail.com>
2023-01-22 23:45:40 -0600

    Página inicial finalizada.

--- small-talk-frontend/src/App.jsx
+++ small-talk-frontend/src/App.jsx
@@ -1,26 +1,8 @@
-import React, { useState } from 'react'
+import React from 'react'
+import LandingPage from './routes/LandingPage'
 
 const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:4000'
 
-const App = () => {
-  const [users, setUsers] = useState()
-
-  const getUsers = async () => {
-    const response = await fetch(`${API_URL}/users`)
-    const data = await response.json()
-    setUsers(data)
-  }
-
-  return (
-    <div>
-      <button onClick={getUsers}>Get Users</button>
-      {users && users.map((user) => (
-        <div key={user._id}>
-          <h3>{`${user.username}#${user.tag}`}</h3>
-        </div>
-      ))}
-    </div>
-  )
-}
+const App = () => <LandingPage />
 
 export default App
---
+++ small-talk-frontend/src/routes/LandingPage.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import SmallTalkLogo from '../assets/images/small-talk-logo.png'
+import '../styles/landing-page.sass'
+
+const LandingPage = () => (
+  <main>
+    <div className="icon-container">
+      <img
+        src={SmallTalkLogo}
+        alt="Small Talk Icon"
+      />
+      <h1>Small Talk</h1>
+    </div>
+    <p className="p-intro">
+      Small Talk is a chat application that mainly focus on giving a simple 
+      and intuitive experience to our users withoutsaturating their view with 
+      unnecessary features and confusing options.
+    </p>
+    <button>Log In</button>
+    <p className="p-sign-in">
+      Haven't created an account? Then you might want to <span>Sign In</span>
+    </p>
+  </main>
+)
+
+export default LandingPage
