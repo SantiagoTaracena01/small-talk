c35c281bb3c4feb62ab1fd930a686fd5ae426a30
unknown <staracenapuga@gmail.com>
2023-02-05 00:00:07 -0600

    Finished user provider

---
+++ small-talk-frontend/src/providers/UserProvider.jsx
@@ -0,0 +1,18 @@
+import React, { createContext, useState, useMemo } from 'react'
+
+const UserContext = createContext()
+
+const UserProvider = ({ children }) => {
+  const [user, setUser] = useState()
+
+  const state = useMemo(() => ({ user, setUser }), [user, setUser])
+
+  return (
+    <UserContext.Provider value={state}>
+      {children}
+    </UserContext.Provider>
+  )
+}
+
+export { UserContext }
+export default UserProvider
--- small-talk-frontend/src/routes/MainPage.jsx
+++ small-talk-frontend/src/routes/MainPage.jsx
@@ -1,5 +1,27 @@
-import React from 'react'
+import React, { useContext, useState, useEffect } from 'react'
+import { UserContext } from '../providers/UserProvider'
 
-const MainPage = () => <h1>Main Page</h1>
+const MainPage = () => {
+  const { user } = useContext(UserContext)
+
+  const [loggedUser, setLoggedUser] = useState({ ...user })
+
+  useEffect(() => {
+    const getUser = async () => {
+      const response = await fetch(`${import.meta.env.VITE_API_URL}/users/${loggedUser._id}`)
+      const data = await response.json()
+      console.log(data)
+    }
+    getUser()
+  }, [])
+
+  return (
+    <div>
+      <h1>Main Page</h1>
+      <p>Welcome, {loggedUser._id}</p>
+      <button onClick={() => console.log("Hello!")}>Click</button>
+    </div>
+  )
+}
 
 export default MainPage
