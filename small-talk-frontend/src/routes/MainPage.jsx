d876f5894b5bc69520025970de2bb0f54cc307d0
MarcoJurado20308 <60157453+MaJu502@users.noreply.github.com>
2023-02-10 23:39:57 -0600

    handle add new contact done

--- small-talk-frontend/src/routes/MainPage.jsx
+++ small-talk-frontend/src/routes/MainPage.jsx
@@ -46,9 +46,19 @@ const dymSuggestions = [
 const MainPage = () => {
   const { user, setUser } = useContext(UserContext)
   //const [dymSuggestions, setDymSuggestions] = useState(false)
-
+  const [users, setUsers] = useState()
   const [isSearchingUser, setIsSearchingUser] = useState(false)
   const [searchedUser, setSearchedUser] = useState(null)
+
+  // get users in db
+  useEffect(() => {
+    const getUsers = async () => {
+      const response = await fetch(`${import.meta.env.VITE_API_URL}/users`)
+      const data = await response.json()
+      setUsers(data)
+    }
+    getUsers()
+  }, [])
 
   const handleLogout = async () => {
     setUser({ })
@@ -62,6 +72,26 @@ const MainPage = () => {
         firstname: null,
         lastname: null,
         password: null,
+        contacts: null,
+        picture: null,
+        logged: false,
+      })
+    })
+  }
+
+  const handleAddContact = async () => {
+    const foundUser = users.find((user) => (user.username === searchedUser))
+    await fetch(`${import.meta.env.VITE_API_URL}/users/${user._id}`, {
+      method: 'PATCH',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        username: null,
+        firstname: null,
+        lastname: null,
+        password: null,
+        contacts: [...user.contacts, foundUser._id],
         picture: null,
         logged: false,
       })
@@ -110,15 +140,23 @@ const MainPage = () => {
       {(isSearchingUser) ? (
         <div className="add-contact-background">
           <div className="add-contact-card">
-            <div className='add-contact-profile-color' />
-            <input className='add-contact-input-user' type="text" id="username-add" placeholder="Username que deseas agregar" onChange={(event) => setSearchedUser(event.target.value)}/>
+            <input
+              className="add-contact-input-user"
+              type="text"
+              id="username-add"
+              placeholder="Username que deseas agregar"
+              onChange={(event) => setSearchedUser(event.target.value)}
+            />
             <p className='DYM-text'>Quieres decir...</p>
             <div className='add-contact-did-you-mean-container'>
               {dymSuggestions.map((suggestion) => (
                 <p className='suggestion-inside-add-contact'>{suggestion.userN}</p>
               ))}
             </div>
-            <button className='close-btn' onClick={() => setIsSearchingUser(false)}>Cerrar</button>
+            <div className='container-btns'>
+              <button className="close-btn" onClick={handleAddContact}>Accept</button>
+              <button className='close-btn' onClick={() => setIsSearchingUser(false)}>Close</button>
+            </div>
           </div>
         </div>
       ) : null}
